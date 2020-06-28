d5c82f7401b4b4a9a8fe142809c535dc90467855
evanhoogmoed <56660574+evanhoogmoed@users.noreply.github.com>
2020-06-28 12:40:56 -0700

    Created Graph showing mutated vs not

--- reumutation.py
+++ reumutation.py
@@ -11,7 +11,31 @@ import numpy as np
 
 #create dataframe from excel file using desired columns and drop empty cells
 df = pd.read_excel(r'C:\Users\emmav\reumutation\dataset1.xlsx',index_col=None,na_values=['NaN'],usecols = "J,K:L")
+#Create bar graph showing those with mutations vs those without
+#calculate total number of samples
+totalsamples = len(df)
+
+
+#calculate total num of mutations
+mutationCount = df.count().sum()
+
+
+#count number of samples with no mutations
+countNan = totalsamples - mutationCount
+
+
+#drop values that are NAN
 df = df.dropna()
+
+#create bar graph
+objects = ('Mutations','No Mutations')
+y_pos = np.arange(len(objects))
+position = [mutationCount,countNan]
+plt.bar(y_pos,position,align='center',alpha = .5,color='b')
+plt.xticks(y_pos,objects)
+plt.ylabel('Num of Samples')
+plt.title('Mutated vs Non-Mutated Samples')
+plt.show
 
 #store df in numpy arr; col 0 = overlappingGene, col 1 = knownCNV, col 2 = repeats
 df_arr = df.to_numpy()
