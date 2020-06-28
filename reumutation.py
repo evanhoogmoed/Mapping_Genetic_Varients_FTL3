f357ea87c9c82ae271867b5aad565dfe6136f44e
evanhoogmoed <56660574+evanhoogmoed@users.noreply.github.com>
2020-06-28 09:10:09 -0700

    created df dropped empty cells

--- reumutation.py
+++ reumutation.py
@@ -7,22 +7,21 @@ Created on Fri Jun 26 20:59:34 2020
 
 import pandas as pd
 import matplotlib as plt
-from pandas import DataFrame
 import numpy as np
 
-mydata = pd.read_excel("dataset1.xlsx")
-df = DataFrame(mydata,columns = ['>chr','begin','end','avgNormalalizedCvg','relativeCvg','calledPloidy','calledCNVType','ploidyScore','CNVTypeScore','overlappingGene','knownCNV','repeats','bestLAF','lowLAF','highLAF'])
-df = df.dropna(0)
+#create dataframe from excel file using desired columns and drop empty cells
+df = pd.read_excel(r'C:\Users\emmav\reumutation\dataset1.xlsx',index_col=None,na_values=['NaN'],usecols = "J,K:L")
+df = df.dropna()
 
-print(df)
+
 
 overlap = df['overlappingGene']
 knownCNV = df['knownCNV']
 repeats = df['repeats']
 
-print(overlap)
-print(knownCNV)
-print(repeats)
+#print(overlap)
+#print(knownCNV)
+#print(repeats)
 
 
 
