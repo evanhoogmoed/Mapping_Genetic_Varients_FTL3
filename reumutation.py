70b53d08f422b1e1ec32a34ff2447ecb0b318130
evanhoogmoed <56660574+evanhoogmoed@users.noreply.github.com>
2020-07-06 14:13:15 -0700

    Created new functions and main

--- reumutation.py
+++ reumutation.py
@@ -16,23 +16,52 @@ dfControl = pd.read_excel(r'C:\Users\emmav\reumutation\flt3Controlpop.xlsx',inde
 #store json file into dataframe
 dfCancer = pd.read_json(r'C:\Users\emmav\reumutation\cancermutations.json')
 
+#compare DNA Change between Cancer and Control
+#graph of Cancer
+def basepairChangeCancerSum(dfCancer):
+    dnaChangeCancer = dfCancer['mutation']
+    variationCountA = dnaChangeCancer.str.count('A>').sum()
+    variationCountT = dnaChangeCancer.str.count('T>').sum()
+    variationCountC = dnaChangeCancer.str.count('C>').sum()
+    variationCountG = dnaChangeCancer.str.count('G>').sum()
+
+def basepairChangeControlSum(dfControl):
+    #change control into same format
+    dfControl['dnachange'] = dfControl['Reference'] + '>' + dfControl['Alternate']
+    dnaChangeControl = dfControl['dnachange']
+    variationControlCountA = dnaChangeControl.str.count('A>').sum()
+    variationControlCountT = dnaChangeControl.str.count('T>').sum()
+    variationControlCountC = dnaChangeControl.str.count('C>').sum()
+    variationControlCountG = dnaChangeControl.str.count('G>').sum()
+
 #create frequency chart of mutations in control population
-mutations = df['Annotation']
-missenseCount = mutations.str.count('missense_variant').sum()
-threeprimeCount = mutations.str.count('3_prime_UTR_variant').sum()
-synonCount = mutations.str.count('synonymous_variant').sum()
-frameshiftCount = mutations.str.count('frameshift_variant').sum()
-stopgainCount = mutations.str.count('stop_gained').sum()
-intronCount = mutations.str.count('intron_variant').sum()
-spliceregionCount = mutations.str.count('splice_region_variant').sum()
+def mutationChart(df):
+    mutations = df['Annotation']
+    missenseCount = mutations.str.count('missense_variant').sum()
+    threeprimeCount = mutations.str.count('3_prime_UTR_variant').sum()
+    synonCount = mutations.str.count('synonymous_variant').sum()
+    frameshiftCount = mutations.str.count('frameshift_variant').sum()
+    stopgainCount = mutations.str.count('stop_gained').sum()
+    intronCount = mutations.str.count('intron_variant').sum()
+    spliceregionCount = mutations.str.count('splice_region_variant').sum()
+    fiveprimeCount = mutations.str.count('5_prime_UTR_variant').sum()
 
-f, ax = plt.subplots(figsize=(18,5))
-objects = ('missense','3-prime','synonymous','frameshift','stop-gain','intron','splice-region')
-y_pos = np.arange(len(objects))
-position = [missenseCount,threeprimeCount,synonCount,frameshiftCount,stopgainCount,intronCount,spliceregionCount]
-f = plt.bar(y_pos,position,align='center',alpha = .5,color='b')
-plt.xticks(y_pos,objects)
-plt.ylabel('Num of Samples')
-plt.title('Frequency of Mutations')
-plt.show
 
+    f, ax = plt.subplots(figsize=(18,5))
+    objects = ('missense','3-prime','synonymous','frameshift','stop-gain','intron','splice-region','5-prime')
+    y_pos = np.arange(len(objects))
+    position = [missenseCount,threeprimeCount,synonCount,frameshiftCount,stopgainCount,intronCount,spliceregionCount,fiveprimeCount]
+    f = plt.bar(y_pos,position,align='center',alpha = .5,color='b')
+    plt.xticks(y_pos,objects)
+    plt.ylabel('Num of Samples')
+    plt.title('Frequency of Mutations')
+    plt.show
+
+
+def main():
+    mutationChart(dfControl)
+    basepairChangeCancerSum(dfCancer)
+    basepairChangeControlSum(dfControl)
+    
+if __name__ == "__main__":
+    main()
